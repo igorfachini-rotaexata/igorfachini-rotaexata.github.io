d4fa2ebdc4d61b1bd48fafe6fdce1694753d4783
igorfachini-rotaexata <igor.fachini@rotaexata.com.br>
2018-09-06 09:15:30 -0300

    test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b83f2c183d0d569d9967e73cdc45923.js"
+  "/precache-manifest.75cda87faf2487485fbca785cb3b47c1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "testquasar"});
